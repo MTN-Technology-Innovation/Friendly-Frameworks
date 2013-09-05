36d4e5987f695efaa1417b6bc867b7ee4f038a14
devlinpadayachee <devlinpadayachee@gmail.com>
2013-09-05 17:07:18 +0200

    HTML 5 site skeleton with login

---
+++ login_skeleton(do not change this is  a template)/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>Division by Zero</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf8">
+<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+<form id="login" action="login_controller.php" method="post">
+    <h1>Log In</h1>
+    <fieldset id="inputs">
+        <input id="username" name="username" type="text" placeholder="Username" autofocus required>   
+        <input id="password" name="password" type="password" placeholder="Password" required>
+		
+    </fieldset>
+    <fieldset id="actions">
+        <input type="submit" id="submit" value="SIGN IN">
+       <!-- <a href="">Forgot your password?</a><a href="register.php">Register</a>-->
+		
+    </fieldset>
+	<fieldset id = "links">
+    <a class = "glow" href="http://www.divisionbyzero.wozaonline.co.za">Division by Zero Web Development</a>
+	</fieldset>
+</form>
+
+</body>
+
+</html>
